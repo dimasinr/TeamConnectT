3f54b9342ea59a578b8e917d60593022313ac914
dimasinr <dimas.indra773@gmail.com>
2025-03-20 14:49:45 +0700

    Team Connect

---
+++ TeamConnect/wwwroot/site/script.js
@@ -0,0 +1,17 @@
+﻿window.getDarkMode = () => {
+    return localStorage.getItem("dark-mode") === "enabled";
+};
+
+window.setDarkMode = (isDark) => {
+    if (isDark) {
+        document.body.classList.add("dark-mode");
+        document.getElementById('header').classList.add("dark-mode-header");
+        document.getElementById('features').classList.add("dark-mode-feat");
+        localStorage.setItem("dark-mode", "enabled");
+    } else {
+        document.getElementById('header').classList.remove("dark-mode-header");
+        document.getElementById('features').classList.remove("dark-mode-feat");
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("dark-mode", "disabled");
+    }
+};
